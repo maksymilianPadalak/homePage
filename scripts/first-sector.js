7f633347a6ad3ebc8140a1e26fbb2f4081e2185c
Maksymilian Padalak <mpadalak@edu.cdv.pl>
2021-04-27 23:29:38 +0200

    11th commit
    Work in progress

--- scripts/first-sector.js
+++ scripts/first-sector.js
@@ -1,10 +1,10 @@
 gsap.registerPlugin(ScrollTrigger)
 
 gsap.to('.first-sector-text-wrapper h1', {
-  scrollTrigger: { trigger: ".first-sector-text-wrapper h1", toggleActions: "restart pause none none"}, x: 300})
+  scrollTrigger: { trigger: ".first-sector-text-wrapper h1", toggleActions: "play complete reverse reset"}, x: 300})
 
 gsap.to('.first-sector-description-wrapper', {
-  scrollTrigger: { trigger: ".first-sector-description-wrapper", toggleActions: "restart pause none none"}, display: "block", opacity: 1, duration: 3})
+  scrollTrigger: { trigger: ".first-sector-description-wrapper", toggleActions: "play complete reverse reset"}, display: "block", opacity: 1, duration: 1})
 
 gsap.to('.first-sector-requirements-wrapper', {
-  scrollTrigger: { trigger: ".first-sector-requirements-wrapper", start: "top center", toggleActions: "restart none none none"}, x: 1000})
+  scrollTrigger: { trigger: ".first-sector-description-wrapper", start: "top center", toggleActions: "play complete reverse reset"}, x: 1000})
