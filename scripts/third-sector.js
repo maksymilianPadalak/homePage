ac836b4b558bc83730d8decce6ea1017d2a0f0c2
Maksymilian Padalak <mpadalak@edu.cdv.pl>
2021-04-28 17:20:15 +0200

    Xth commit
    Major changes and additions

--- scripts/third-sector.js
+++ scripts/third-sector.js
@@ -1,2 +1,11 @@
-gsap.to('.third-sector-text-wrapper', {
-  scrollTrigger: { trigger: ".third-sector-text-wrapper",  start: "90% 90%", toggleActions: "play complete reverse reset"}, opacity: 1, duration: 1.5, ease: "bounce.out", x: 500})
+gsap.to(".third-sector-text-wrapper", {
+  scrollTrigger: {
+    trigger: ".third-sector-text-wrapper",
+    start: "90% 90%",
+    toggleActions: "play complete reverse reset",
+  },
+  opacity: 1,
+  duration: 1.5,
+  ease: "bounce.out",
+  x: 500,
+});
