d9a6fe8d4b0dd0254cb0d055edc3778a1d2a7d8a
Maksymilian Padalak <mpadalak@edu.cdv.pl>
2021-04-29 20:58:30 +0200

    Form
    form bug

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,39 +1,78 @@
 //functions that reveals small text when something is typed
 
-const nameInput = document.getElementById('name-input')
-const surnameInput = document.getElementById('surname-input')
-const emailInput = document.getElementById('email-input')
-const messageInput = document.getElementById('message-input')
+const nameInput = document.getElementById("name-input");
+const surnameInput = document.getElementById("surname-input");
+const emailInput = document.getElementById("email-input");
+const messageInput = document.getElementById("message-input");
 
+const nameInputWrapper = document.getElementById("name-input-wrapper");
+const surnameInputWrapper = document.getElementById("surname-input-wrapper");
+const emailInputWrapper = document.getElementById("email-input-wrapper");
+const messageInputWrapper = document.getElementById("message-input-wrapper");
 
 nameInput.addEventListener("input", () => {
-  if(nameInput.value.length === 0){
-    gsap.to('.name-while-input', {display: 'none'})
-  } else{
-    gsap.to('.name-while-input', {display: 'block'})
+  if (nameInput.value.length === 0) {
+    gsap.to(".name-while-input", { display: "none" });
+  } else {
+    gsap.to(".name-while-input", { display: "block" });
   }
 });
 
 surnameInput.addEventListener("input", () => {
-  if(surnameInput.value.length === 0){
-    gsap.to('.surname-while-input', {display: 'none'})
-  } else{
-    gsap.to('.surname-while-input', {display: 'block'})
+  if (surnameInput.value.length === 0) {
+    gsap.to(".surname-while-input", { display: "none" });
+  } else {
+    gsap.to(".surname-while-input", { display: "block" });
   }
 });
 
 emailInput.addEventListener("input", () => {
-  if(emailInput.value.length === 0){
-    gsap.to('.email-while-input', {display: 'none'})
-  } else{
-    gsap.to('.email-while-input', {display: 'block'})
+  if (emailInput.value.length === 0) {
+    gsap.to(".email-while-input", { display: "none" });
+  } else {
+    gsap.to(".email-while-input", { display: "block" });
   }
 });
 
 messageInput.addEventListener("input", () => {
-  if(messageInput.value.length === 0){
-    gsap.to('.message-while-input', {display: 'none'})
-  } else{
-    gsap.to('.message-while-input', {display: 'block'})
+  if (messageInput.value.length === 0) {
+    gsap.to(".message-while-input", { display: "none" });
+  } else {
+    gsap.to(".message-while-input", { display: "block" });
   }
 });
+
+// form validation
+const validateForm = () => {
+  if (nameInput.value.trim() === "") {
+    nameInputWrapper.style.border = "2px solid red";
+    return false;
+  }
+  if (nameInput.value.trim() !== "") {
+    nameInputWrapper.style.border = "none";
+  }
+
+  if (surnameInput.value.trim() === "") {
+    surnameInputWrapper.style.border = "2px solid red";
+    return false;
+  }
+  if (surnameInput.value.trim() !== "") {
+    surnameInputWrapper.style.border = "none";
+  }
+
+  if (emailInput.value.trim() === "") {
+    emailInputWrapper.style.border = "2px solid red";
+    return false;
+  }
+  if (emailInput.value.trim() !== "") {
+    emailInputWrapper.style.border = "none";
+  }
+
+  if (messageInput.value.trim() === "") {
+    messageInputWrapper.style.border = "2px solid red";
+    return false;
+  }
+  if (messageInput.value.trim() !== "") {
+    messageInputWrapper.style.border = "none";
+  }
+};
--- scripts/second-sector.js
+++ scripts/second-sector.js
@@ -1,3 +1,5 @@
+import {sendHttpRequest} from './http-Requestscripts/'
+
 gsap.registerPlugin(ScrollTrigger)
 
 gsap.to('.second-sector-text-wrapper h1', {
@@ -6,3 +8,4 @@ gsap.to('.second-sector-text-wrapper h1', {
 gsap.to('.second-sector-text-wrapper p', {
   scrollTrigger: { trigger: ".second-sector-text-wrapper p", toggleActions: "play complete reverse reset"}, opacity: 1, duration: 2, delay: 1})  
   
+console.log(sendHttpRequest)
