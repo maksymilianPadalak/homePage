3e43d2ee63af15b91fce3f5e2a2f728250b6b5a3
Maksymilian Padalak <mpadalak@edu.cdv.pl>
2021-04-29 16:46:59 +0200

    Contact desktop done
    Text area to fix

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,15 +1,39 @@
-const nameInput = document.querySelector(".contact-input-wrapper");
+//functions that reveals small text when something is typed
 
-console.log(nameInput)
+const nameInput = document.getElementById('name-input')
+const surnameInput = document.getElementById('surname-input')
+const emailInput = document.getElementById('email-input')
+const messageInput = document.getElementById('message-input')
+
 
 nameInput.addEventListener("input", () => {
-
+  if(nameInput.value.length === 0){
+    gsap.to('.name-while-input', {display: 'none'})
+  } else{
     gsap.to('.name-while-input', {display: 'block'})
-
+  }
 });
 
-nameInput.addEventListener('blur', () => {
-  if(nameInput.Value === ''){
-    gsap.to('.name-while-input', {display: 'none'})
+surnameInput.addEventListener("input", () => {
+  if(surnameInput.value.length === 0){
+    gsap.to('.surname-while-input', {display: 'none'})
+  } else{
+    gsap.to('.surname-while-input', {display: 'block'})
   }
-})
+});
+
+emailInput.addEventListener("input", () => {
+  if(emailInput.value.length === 0){
+    gsap.to('.email-while-input', {display: 'none'})
+  } else{
+    gsap.to('.email-while-input', {display: 'block'})
+  }
+});
+
+messageInput.addEventListener("input", () => {
+  if(messageInput.value.length === 0){
+    gsap.to('.message-while-input', {display: 'none'})
+  } else{
+    gsap.to('.message-while-input', {display: 'block'})
+  }
+});
